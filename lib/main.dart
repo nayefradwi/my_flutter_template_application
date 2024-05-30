3c34326846bbe43fe23f251fb9f2e9d09c86f220
nayefradwi <naif.mazen.radwi@gmail.com>
2024-05-30 21:41:46 +0300

    implemented main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:my_flutter_template_application/core/bootstrap.dart';
+import 'package:my_flutter_template_application/ui/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  bootstrap(() => const MainApp());
+}
