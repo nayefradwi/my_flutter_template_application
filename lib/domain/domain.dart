35bfa2009f1506bfe050a9313204ead4017ee003
nayefradwi <naif.mazen.radwi@gmail.com>
2024-05-30 20:42:49 +0300

    exporting domains to be as if they are a package

---
+++ lib/domain/domain.dart
@@ -0,0 +1,8 @@
+export 'enums/example.dart';
+export 'mappers/example.dart';
+export 'models/example.dart';
+export 'services/device_info_service.dart';
+export 'use_cases/has_update_use_case.dart';
+export 'use_cases/settings/get_language_use_case.dart';
+export 'use_cases/settings/set_language_use_case.dart';
+export 'utils/functions.dart';
